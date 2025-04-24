a8b072b726ea206b7d419c380f3ec6c4a50880b2
Vladislav1102 <darknoem921@gmail.com>
2025-04-24 13:48:18 +0300

    First basic version

--- proxy.cpp
+++ proxy.cpp
@@ -8,6 +8,7 @@ Proxy::Proxy() {}
 Proxy::~Proxy() 
 {
     close(socket_client);
+    close(connect_client);
     close(socket_target);
 }
 
@@ -26,13 +27,12 @@ int32_t Proxy::open_socket()
         std::cerr << "Error set settings socket" << std::endl;
         return EXIT_FAILURE;
     }
-    
     return socketfd;
 }
 
-void Proxy::install_settings_sockets(struct sockaddr_in* device, in_addr_t* address)
+void Proxy::install_settings_sockets(struct sockaddr_in* device, in_addr_t& address, uint16_t port)
 {
-    device->sin_addr.s_addr = htonl(*address);
+    device->sin_addr.s_addr = address;
     device->sin_family      = AF_INET;
     device->sin_port        = htons(port);
 }
@@ -41,59 +41,98 @@ void Proxy::setup_client()
 {
     this->socket_client = open_socket();
 
-    install_settings_sockets(&client_addr, &address_client);
+    install_settings_sockets(&client_addr, address_client, port);
     memset(&(client_addr.sin_zero), '\0', 8);
 
     if (bind(socket_client, (struct sockaddr *)&client_addr, sizeof(struct sockaddr)) < 0)
     {
-        std::cerr << "Error binding socket" << std::endl;
+        std::cerr << "Error binding socket for client" << std::endl;
     }
 
     listen(socket_client, SOMAXCONN);
 }
 
-void Proxy::setup_target(const std::string& input_addres)
+void Proxy::setup_target()
 {
     this->socket_target = open_socket();
 
-    inet_pton(AF_INET, input_addres.c_str(), &address_target);
-    install_settings_sockets(&target_addr, &address_target);
+    std::string input_addres;
+    std::cout << "Введите адрес целевого сервера" << std::endl;
+    std::cin >> input_addres;
+
+    if (inet_pton(AF_INET, input_addres.c_str(),&address_target) <= 0) 
+    {
+        std::cerr << "Error inet_pton" << std::endl;
+    }
+    install_settings_sockets(&target_addr, address_target, port_target);
 }
 
 void Proxy::connect_client_to_server()
 {
-    this->socket_client = accept(socket_target, nullptr, nullptr);
+    connect_client = accept(socket_client, nullptr, nullptr);
+    if (connect_client <= 0)
+    {
+        std::cerr << "Error accept client to proxy" << std::endl;
+    }
 
     if (connect(socket_target, (struct sockaddr*)&target_addr, sizeof(target_addr)) < 0)
     {
-        std::cerr << "Error connecting client to server" << std::endl;
+        std::cerr << "Error connecting proxy to target" << std::endl;
         close(socket_client);
+    } else {
+        std::cout << "Connect client for target server succsses" << std::endl;
     }
+
+    std::cout << "Client accept to target" << std::endl;
 }
 
-void Proxy::poller()
+void Proxy::transfer_data()
 {
-    char data[MAX_SIZE] = {};
-    size_t size_data;
+    char data_client[MAX_SIZE] = {};
+    char data_target[MAX_SIZE] = {};
+    struct pollfd fds[2];
+    fds[0].fd = connect_client;
+    fds[0].events = POLLIN;
 
-    while ((size_data = read(socket_client, data, sizeof(data))) > 0)
+    fds[1].fd = socket_target;
+    fds[1].events = POLLIN;
+
+    while (true) 
     {
-        send(socket_target, data, size_data, 0);
-    }
+        int ret = poll(fds, 2, -1);
+
+        if (ret < 0)
+        {
+            std::cerr << "Error poll" << std::endl;
+        }
+
+        if (fds[0].revents & POLLIN)
+        {
+            ssize_t len = read(connect_client, data_client, sizeof(data_client));
+            if (len <= 0) break;
+            send(socket_target, data_client, len, 0);
+            std::cout << "[CLIENT: ]" << std::string(data_client, len);
+        }
+
+        if (fds[1].revents & POLLIN)
+        {
+            ssize_t len = read(socket_target, data_target, sizeof(data_target));
+            if (len <= 0) break;
+            send(connect_client, data_target, len, 0);
+            std::cout << "[SERVER: ]"<< std::string(data_target, len);
+        }
+    } 
 }
 
 void Proxy::run_proxy()
 {
     setup_client();
 
-    std::string input_addres;
-    std::cout << "Введите адрес целевого сервера" << std::endl;
-    std::cin >> input_addres;
-
-    setup_target(input_addres);
+    setup_target();
+    connect_client_to_server();
 
     while(true)
     {
-        poller();
+        transfer_data();
     }
 }
--- proxy.hpp
+++ proxy.hpp
@@ -10,9 +10,11 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <csignal>
+#include <poll.h>
 
 static constexpr uint16_t port = 9000;
-static constexpr uint16_t MAX_SIZE = 4096;
+static constexpr uint16_t MAX_SIZE = 4;
+static constexpr uint16_t port_target = 1234;
 
 class Proxy {
     
@@ -24,20 +26,21 @@ public:
 private:
     int32_t open_socket();
     void setup_client();
-    void setup_target(const std::string& input_addres);
-    void poller();
-    void install_settings_sockets(struct sockaddr_in* device, in_addr_t* address);
+    void setup_target();
+    void transfer_data();
+    void install_settings_sockets(struct sockaddr_in* device, in_addr_t& address, uint16_t port);
     void connect_client_to_server();
     void signal_handler();
 
     struct sockaddr_in client_addr, target_addr;
 
-    in_addr_t address_client   = INADDR_ANY;
-    in_addr_t address_target = 0;
+    in_addr_t address_client = INADDR_ANY;
+    in_addr_t address_target;
 
     uint8_t opt = 1;
     int32_t socket_client;
     int32_t socket_target;
+    int32_t connect_client;
 };
 
 #endif //PROXY_HPP
