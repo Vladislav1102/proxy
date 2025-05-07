93b6e57afb47f984a5cdb6b7e23f40a599a0f38b
Пичугин Владислав Александрович <Vladislav.Pichugin@incomsystem.ru>
2025-05-07 07:51:18 +0300

    Add namespace

--- proxy.cpp
+++ proxy.cpp
@@ -1,8 +1,36 @@
 #include "proxy.hpp"
 
+namespace
+{
+uint16_t open_socket()
+{
+    uint8_t optval = 1;
+    uint16_t new_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (new_socket <= 0)
+    {
+        std::cerr << "Error open socket" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (setsockopt(new_socket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(uint16_t)) < 0)
+    {
+        std::cerr << "Error set socket" << std::endl;
+        return EXIT_FAILURE;
+    }
+    return new_socket;
+}
+
+void settings_socket(struct sockaddr_in* device, in_addr_t& address, int16_t port)
+{
+    device->sin_family      = AF_INET;
+    device->sin_addr.s_addr = address;
+    device->sin_port        = htons(port);
+}
+} //namespace
+
 RealServer::RealServer() 
 {
-    serverfd = open_socket();
+    serverfd = ::open_socket();
 }
 
 RealServer::~RealServer()
@@ -20,5 +48,10 @@ void RealServer::setup()
         std::cerr << "Error inet_pton Server" << std::endl;
     }
 
-    settings_socket(&server_addr, address_server, port_server);
+    ::settings_socket(&server_addr, address_server, port_server);
+}
+
+void RealServer::handler_data()
+{
+
 }
